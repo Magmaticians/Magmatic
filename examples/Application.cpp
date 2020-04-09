ff7a4d83dd3668ff5dbe6008c98771688ed9f1f2
Michal23 <michal.wegrzyn2137@gmail.com>
2020-04-09 23:48:04 +0200

    Tests

---
+++ examples/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 09.04.2020.
+//
+
+#include "Application.hpp"
---
+++ examples/Application.hpp
@@ -0,0 +1,14 @@
+//
+// Created by user on 09.04.2020.
+//
+
+#ifndef MAGMATIC_APPLICATION_H
+#define MAGMATIC_APPLICATION_H
+
+
+class Application {
+
+};
+
+
+#endif //MAGMATIC_APPLICATION_H

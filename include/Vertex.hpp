ed44d14826b9498a386ecdbaa67e7fb1196ec405
Michal23 <michal.wegrzyn2137@gmail.com>
2020-04-14 15:02:51 +0200

    Some memory management

--- include/Vertex.hpp
+++ include/Vertex.hpp
@@ -21,7 +21,7 @@ namespace magmatic {
 		static std::array<vk::VertexInputAttributeDescription, 2> getAttributeDescriptions() {
 			return {
 					vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, position)),
-					vk::VertexInputAttributeDescription(0, 1, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color))
+					vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color))
 			};
 		}
 	};

49868b627d2700552a062587cb73104db18487db
Xert <michal740@interia.eu>
2020-04-17 22:36:21 +0200

    Example cleanup

--- examples/sound_test/main.cpp
+++ examples/sound_test/main.cpp
@@ -6,6 +6,35 @@
 
 using namespace std::chrono_literals;
 
+void orbitingSound(const magmatic::sound::SoundSource& source)
+{
+	constexpr int orbit_size = 30;
+	source.setPosition({0,0, 0});
+	source.play();
+	source.setGain(3);
+	float pos = 0.0;
+	while(source.state() == magmatic::sound::SoundSource::Playing)
+	{
+		source.setPosition({orbit_size*std::sin(pos), orbit_size*std::cos(pos), 0});
+		std::this_thread::sleep_for(200ms);
+		pos += 0.3;
+	}
+}
+
+void fadingSound(const magmatic::sound::SoundSource& source)
+{
+	source.setPosition({0,0, 0});
+	source.play();
+	source.setGain(1);
+	float pos = 0.0;
+	while(source.state() == magmatic::sound::SoundSource::Playing)
+	{
+		source.setPosition({pos, pos, 0});
+		std::this_thread::sleep_for(200ms);
+		pos += 0.3;
+	}
+}
+
 int main(int argc, char** argv)
 {
 	magmatic::sound::SoundDevice device;
@@ -14,24 +43,7 @@ int main(int argc, char** argv)
 	magmatic::sound::SoundSource source({0, 0, 0});
 	//https://freesound.org/people/hmmm101/sounds/335521/
 	source.setSound(buffer);
-	source.play();
-	float pos = 0;
-//	while(source.state() == magmatic::sound::SoundSource::Playing)
-//	{
-//		source.setPosition({pos, pos, pos});
-//		pos += 0.3;
-//		std::this_thread::sleep_for(200ms);
-//	}
-	source.setPosition({0,0, 0});
-	source.play();
-	while(source.state() == magmatic::sound::SoundSource::Playing)
-	{
-		source.setPosition({20*std::sin(pos), 20*std::cos(pos), 0});
-		std::this_thread::sleep_for(200ms);
-		pos += 0.3;
-	}
 
-	source.setSound(buffer2);
-	source.play();
-	source.wait();
+	orbitingSound(source);
+	fadingSound(source);
 }

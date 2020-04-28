c51706cf8e64349543bad42784d89f3ad6d039d7
Xert <michal740@interia.eu>
2020-04-28 10:17:51 +0200

    Sampler, texture write descriptor info

--- include/render/Sampler.hpp
+++ include/render/Sampler.hpp
@@ -1,8 +1,10 @@
 #ifndef MAGMATIC_SAMPLER_HPP
 #define MAGMATIC_SAMPLER_HPP
 
-
 #include <vulkan/vulkan.hpp>
+#include "DescriptorWriteUpdate.hpp"
+
+
 namespace magmatic::render
 {
 	class Sampler
@@ -10,6 +12,18 @@ namespace magmatic::render
 		friend class LogicalDevice;
 	public:
 		const vk::UniqueSampler sampler;
+
+		[[nodiscard]] DescriptorWriteUpdate getWriteInfo(size_t dst_binding, size_t dst_array_elem) const
+		{
+			DescriptorWriteUpdate update;
+			update.type = DescriptorWriteUpdate::eSampler;
+			update.dst_binding = dst_array_elem;
+			update.dst_array_elem = dst_array_elem;
+			update.data_info = vk::DescriptorImageInfo {
+				sampler.get()
+			};
+			return update;
+		}
 
 	private:
 		explicit Sampler(vk::UniqueSampler sampler) : sampler(std::move(sampler)) {};
--- include/render/Texture.hpp
+++ include/render/Texture.hpp
@@ -2,6 +2,7 @@
 #define MAGMATIC_TEXTURE_HPP
 
 #include <vulkan/vulkan.hpp>
+#include "DescriptorWriteUpdate.hpp"
 
 
 namespace magmatic::render
@@ -13,6 +14,20 @@ namespace magmatic::render
 		const vk::UniqueImage image;
 		const vk::UniqueImageView image_view;
 		const vk::UniqueDeviceMemory image_memory;
+
+		[[nodiscard]] DescriptorWriteUpdate getWriteInfo(size_t dst_binding, size_t dst_array_elem) const
+		{
+			DescriptorWriteUpdate update;
+			update.type = DescriptorWriteUpdate::eImage;
+			update.dst_binding = dst_array_elem;
+			update.dst_array_elem = dst_array_elem;
+			update.data_info = vk::DescriptorImageInfo {
+					{},
+					image_view.get(),
+					vk::ImageLayout::eShaderReadOnlyOptimal
+			};
+			return update;
+		}
 
 	private:
 		Texture(vk::UniqueImage image, vk::UniqueImageView view, vk::UniqueDeviceMemory memory)
